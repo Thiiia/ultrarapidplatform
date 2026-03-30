62f64ac9eceee0e97f7b1b52565135295a2b9a4e
kam7713Thiiia <kameron@thiiia.com>
2026-03-30 13:05:47 -0500

    Updated app/page.tsx to remove errors.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { redirect } from "next/navigation";
-import { auth0 } from "@/lib/auth0";
+import { getAuth0 } from "@/lib/auth0";
 
 export default async function HomePage() {
-  const session = await auth0.getSession();
+  const session = await getAuth0().getSession();
 
   if (!session?.user) {
     redirect("/login");
