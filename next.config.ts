dc9d086e09c6e268713059e6ade2e0e0cf7ad2f4
kam7713Thiiia <kameron@thiiia.com>
2026-04-05 19:15:28 -0500

    Debugging Launcher (enabled LFS on Vercel)

--- next.config.ts
+++ next.config.ts
@@ -13,28 +13,16 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/unity/Build/:file(.*\\.unityweb)",
-        headers: [
-          { key: "Content-Encoding", value: "br" },
-        ],
-      },
-      {
         source: "/unity/Build/:file(.*\\.wasm\\.unityweb)",
-        headers: [
-          { key: "Content-Type", value: "application/wasm" },
-        ],
+        headers: [{ key: "Content-Type", value: "application/wasm" }],
       },
       {
         source: "/unity/Build/:file(.*\\.framework\\.js\\.unityweb)",
-        headers: [
-          { key: "Content-Type", value: "application/javascript" },
-        ],
+        headers: [{ key: "Content-Type", value: "application/javascript" }],
       },
       {
         source: "/unity/Build/:file(.*\\.data\\.unityweb)",
-        headers: [
-          { key: "Content-Type", value: "application/octet-stream" },
-        ],
+        headers: [{ key: "Content-Type", value: "application/octet-stream" }],
       },
     ];
   },
